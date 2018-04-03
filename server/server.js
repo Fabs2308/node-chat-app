488738d7061c8a921ddbdc46476b8fbabf681440
Fabs2308 <fabse231@googlemail.com>
2018-04-03 15:07:06 +0200

    false server.js

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const publicPath = path.join(__dirname, '../public');
 var app = express();
 
 const port = process.env.PORT || 3000;
-d
+
 app.use(express.static(publicPath));
 
 app.listen(port, () => {
