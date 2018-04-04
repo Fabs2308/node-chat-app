f97f4b21cbfd2689bcc3e96f3c196f92904e55b3
Fabs2308 <fabse231@googlemail.com>
2018-04-04 13:35:19 +0200

    4/4/2018

--- server/server.js
+++ server/server.js
@@ -1,14 +1,33 @@
 const path = require('path');
-const express = require('express')
-const bodyParser = require('body-parser');
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
+
 const publicPath = path.join(__dirname, '../public');
-
-var app = express();
-
 const port = process.env.PORT || 3000;
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
-    console.log(`Started on port ${port}`);
-}).on('error', console.log);
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        })
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is up on ${port}`);
+});
