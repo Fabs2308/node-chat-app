e419fd6e62e7fc978de7a572be95629484fce03b
Fabs2308 <fabse231@googlemail.com>
2018-04-05 00:41:41 +0200

    4/5/2018

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
     it('should generate correct message object', () => {
@@ -8,5 +8,16 @@ describe('generateMessage', () => {
 
         expect (typeof message.createdAt).toBe('number');
         expect (message).toMatchObject({from: "me", text: "Hey there!"});
+    });
+});
+
+describe('generateLocationMessage', () => {
+    it('should generate correct location object', () => {
+        var latitude = 15;
+        var longitude = 19;
+        var messageLocation = generateLocationMessage("me", latitude, longitude);
+
+        expect (typeof messageLocation.createdAt).toBe('number');
+        expect (messageLocation).toMatchObject({from: "me", url: `https://www.google.com/maps?q=${latitude}, ${longitude}`});
     });
 });
